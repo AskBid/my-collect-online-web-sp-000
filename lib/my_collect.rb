d3d5de04c5999b73721494469516e1a257c92494
sergio marelli <sergio.mrll@gmail.com>
2019-11-04 21:44:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,14 @@
+require pry
+
 def my_collect(array)
   i = 0
   collection = []
   while i < array.size
     i += 1
+    binding.pry
     collection << yield(array[i])
   end
   collection
 end
+
+my_collect(['ruby', 'javascript', 'python', 'objective-c'])
